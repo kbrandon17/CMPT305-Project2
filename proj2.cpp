4f3f5b2acbfe3c9398e05aba29752faa822f90c1
kbrandon17 <kylebrandon17@gmail.com>
2022-04-14 19:08:43 -0700

    tiny changes

--- proj2.cpp
+++ proj2.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string>
 
-int main() {
-    printf("");
+int main(int argc, char argv[]) {
+    if (argc != 5) {
+        printf("Incorrect number of arguments provided. Exiting now!\n");
+        exit(1);
+    }
+
+    std::string traceFileName = "test";
+    int startInstruction = argv[2];
+    int instructionCount = argv[3]; 
+    int pipelineWidth = argv[4];
+
     return 0;
 }
